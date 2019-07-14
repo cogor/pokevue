f80e57c9f7fd2ebb280f59301a60676e12a12d8f
Ilya <ilya.sosidka@gmail.com>
2019-07-14 12:15:08 +0300

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/pokemon_PNG92.webp">
-    <h1>Pokemon's list with Vue.js</h1>
-    <div class="list">
-      <div class="item" v-for="item in list">
-        <h3>{{item.name}}</h3>
-      </div>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-  import Axios from 'axios';
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      list: {},
-      errors: [],
-    }
-  },
-  mounted() {
-    Axios.get('https://pokeapi.co/api/v2/pokemon/?limit=30')
-            .then( res => {
-              this.list = res.data.results;
-            })
-            .catch(err => {
-              this.errors.push = err
-            })
-  }
+  name: 'app'
 }
 </script>
 
@@ -44,36 +20,4 @@ export default {
   padding-left: 16px;
   padding-right: 16px;
 }
-  img{
-    max-width: 200px;
-    height: auto;
-  }
-  h1{
-    font-weight: 600;
-    font-size: 60px;
-  }
-  h3{
-    text-transform: capitalize;
-  }
-  .list{
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-  }
-  .item{
-    width: 25%;
-    padding: 16px 32px;
-    background: #35495e;
-    color: #42b983;
-    border: 3px solid #42b983;
-    border-radius: 5px;
-    margin-top: 8px;
-    margin-bottom: 8px;
-    cursor: pointer;
-    transition: all .3s;
-  }
-  .item:hover{
-    background: aliceblue;
-    color: #42b983;
-  }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="../assets/pokemon_PNG92.webp">
+        <h1>Pokemon's list with Vue.js</h1>
+        <div class="list">
+            <div class="item" v-for="(item, index) in list">
+                <h3>{{item.name}}</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Axios from 'axios';
+
+    export default {
+        name: 'home',
+        data() {
+            return {
+                list: {},
+                errors: [],
+            }
+        },
+        mounted() {
+            Axios.get('https://pokeapi.co/api/v2/pokemon/?limit=30')
+                .then( res => {
+                    this.list = res.data.results;
+                })
+                .catch(err => {
+                    this.errors.push = err
+                })
+        }
+    }
+</script>
+
+<style>
+    img{
+        max-width: 200px;
+        height: auto;
+    }
+    h1{
+        font-weight: 600;
+        font-size: 60px;
+    }
+    h3{
+        text-transform: capitalize;
+    }
+    .list{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+    .item{
+        width: 25%;
+        padding: 16px 32px;
+        background: #35495e;
+        color: #42b983;
+        border: 3px solid #42b983;
+        border-radius: 5px;
+        margin-top: 8px;
+        margin-bottom: 8px;
+        cursor: pointer;
+        transition: all .3s;
+    }
+    .item:hover{
+        background: aliceblue;
+        color: #42b983;
+    }
+    .item:hover a{
+        color: #42b983;
+    }
+    a{
+        color: aliceblue;
+    }
+</style>
